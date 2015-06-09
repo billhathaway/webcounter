29656b064c5bd1e2f1395d33394a85b92e13fe3f
Bill Hathaway <bill.hathaway@gmail.com>
2015-06-08 20:49:38 -0700

    minor test formatting

--- webcounter_test.go
+++ webcounter_test.go
@@ -12,6 +12,7 @@ import (
 
 var server *httptest.Server
 
+// init sets up our handler on a test instance
 func init() {
 	wc, err := New()
 	if err != nil {
@@ -19,6 +20,9 @@ func init() {
 	}
 	server = httptest.NewServer(wc)
 }
+
+// TestInvalidRequests will verify proper responses to requests we don't support
+// including invalid URLs and request methods
 func TestInvalidRequests(t *testing.T) {
 	urls := []string{"/", "/favicon.ico"}
 	for _, url := range urls {
@@ -31,7 +35,7 @@ func TestInvalidRequests(t *testing.T) {
 			t.Errorf("expected %d for %s but received %d\n", http.StatusNotFound, url, resp.StatusCode)
 			continue
 		}
-		t.Logf("received 404 as expected for %s\n", url)
+		t.Logf("received %d as expected for %s\n", http.StatusNotFound, url)
 		resp.Body.Close()
 	}
 	url := server.URL + "/a.txt"
@@ -47,30 +51,7 @@ func TestInvalidRequests(t *testing.T) {
 
 }
 
-func expectCount(url string, referer string, val int) error {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Referer", referer)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("error making request to %s - %s\n", url, err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("expected status 200 for GET to %s received %d\n", url, resp.StatusCode)
-	}
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response body for %s - %s\n", url, err)
-	}
-	stringVal := strconv.Itoa(val)
-	if string(content) != stringVal {
-		return fmt.Errorf("expected content %d but received %s\n", val, string(content))
-	}
-	return nil
-}
-
+// TestWebcounterValues validates the basic ounter works including resets
 func TestWebcounterValues(t *testing.T) {
 	id := "aa.txt"
 	url := server.URL + "/" + id
@@ -93,7 +74,7 @@ func TestWebcounterValues(t *testing.T) {
 		t.Fatalf("DELETE expected status 200 but received - %d\n", resp.StatusCode)
 	}
 	t.Logf("reset counter for %s\n", id)
-	for i := 0; i < 11; i++ {
+	for i := 0; i < 3; i++ {
 		if err := expectCount(url, "", i+1); err != nil {
 			t.Error(err)
 		} else {
@@ -130,6 +111,7 @@ func TestSuffixes(t *testing.T) {
 	}
 }
 
+// TestReferers validates that counts are kept separate per HTTP referer
 func TestReferers(t *testing.T) {
 	tests := []struct {
 		referer string
@@ -148,4 +130,29 @@ func TestReferers(t *testing.T) {
 			t.Error(err)
 		}
 	}
+}
+
+// expectCount is a helper function to see if the number returned is correct
+func expectCount(url string, referer string, val int) error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Referer", referer)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error making request to %s - %s\n", url, err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("expected status 200 for GET to %s received %d\n", url, resp.StatusCode)
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body for %s - %s\n", url, err)
+	}
+	stringVal := strconv.Itoa(val)
+	if string(content) != stringVal {
+		return fmt.Errorf("expected content %d but received %s\n", val, string(content))
+	}
+	return nil
 }
